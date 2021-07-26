0a1fb84827e5a8bc557ed060133b8a5bccd43a4b
Ekaterina Bogdanova <bekaterina688@gmail.com>
2021-07-26 07:26:25 +0300

    2.2-object-prototype: tasks 1, 2 added

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,23 @@
-//String.prototype.isPalindrome - для задачи №1
+// Задача №1
+String.prototype.isPalindrome = function(phrase) {
+    let strPhrase = phrase.toUpperCase().split('');
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    strPhrase.filter((item, index) => {
+        if (item == ' ' || item === ',' || item === '-' || item === ':' || item === '.' || item === '!' || item === '?') strPhrase.splice(index, 1)});
+
+        let lengthString = strPhrase.length;
+        return strPhrase.every((item, index) => item === strPhrase[lengthString-1-index]);         
 }
 
-function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+// Задача №2
+function getAverageMark(marks) {    
+    let total = 0;
+
+    for (let i = 0; i < marks.length; i++) {
+        total += marks[i];
+    }
+
+    let average = total / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
 }
